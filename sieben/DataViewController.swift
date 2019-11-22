ad576fb15edf874c8cb7a0b576cc26e6b6229f6b
saulotdr <saulotdr@gmail.com>
2019-11-22 00:56:23 -0300

    Initial Commit

---
+++ sieben/DataViewController.swift
@@ -0,0 +1,29 @@
+//
+//  DataViewController.swift
+//  sieben
+//
+//  Created by Saulo Teodoro on 22/11/2019.
+//  Copyright © 2019 docsystem.sieben.com. All rights reserved.
+//
+
+import UIKit
+
+class DataViewController: UIViewController {
+
+    @IBOutlet weak var dataLabel: UILabel!
+    var dataObject: String = ""
+
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(animated)
+        self.dataLabel!.text = dataObject
+    }
+
+
+}
+
